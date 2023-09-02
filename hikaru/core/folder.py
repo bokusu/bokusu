49596ada8d8b2bafd545b219b606105d96845f38
Sultan Iskandar Maulana <nattadasu@nattadasu.my.id>
2023-09-02 19:50:27 +0000

    Update folder module

--- hikaru/core/folder.py
+++ hikaru/core/folder.py
@@ -1,6 +1,21 @@
-import os
+"""This module contains functions for setting up folders."""
 
-def add_directory(path: str, name: str) -> None:
+import os
+from typing import Optional
+
+def add_directory(path: str, name: Optional[str] = None) -> None:
+    """
+    Creates a directory if it doesn't exist.
+    
+    :param path: The path to the directory.
+    :type path: str
+    :param name: The name of the directory. If None, the path will be used.
+    :type name: str | None
+    :return: Should return None.
+    :rtype: None
+    """
     print(f"Creating directory for {name}")
     if not os.path.exists(path):
         os.makedirs(path)
+    else:
+        print(f"Directory for {name} already exists.")
