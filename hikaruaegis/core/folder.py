e8e804b68c2129f8fbea6ff7835ac2814fa93a1e
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-12-14 22:22:01 +0700

    Resolve merge conflict during stashing

--- hikaruaegis/core/folder.py
+++ hikaruaegis/core/folder.py
@@ -2,54 +2,59 @@
 
 import os
 from platform import system
-from typing import Optional
 
-def add_directory(path: str, name: Optional[str] = None) -> None:
+def get_hikaru_root() -> str:
+    """
+    Get the root directory of hikaruaegis.
+
+    On Windows, it should be %USERPROFILE%\\\\.hikaruaegis, but on POSIX, it should be ~/.hikaruaegis.
+    However, on CI/CD (GitHub Actions), it should be current repo directory.
+
+    :return: The path to the root directory.
+    :rtype: str
+    """
+    # check if the program runs on Windows
+    if system() == "Windows":
+        # get the user profile directory
+        user_profile = os.getenv("USERPROFILE")
+        # check if the user profile directory exists
+        if user_profile:
+            # return the path to the root directory
+            return os.path.join(user_profile, ".hikaruaegis")
+        else:
+            # return the path to the root directory
+            return os.path.join(os.getcwd(), ".hikaruaegis")
+    else:
+        if os.getenv("GITHUB_ACTIONS"):
+            return os.getcwd()
+        else:
+            # resolve the home directory
+            home = os.path.expanduser("~")
+            # return the path to the root directory
+            return os.path.join(home, ".hikaruaegis")
+
+def add_directory(*path: str, name: str | None = None) -> str:
     """
     Creates a directory if it doesn't exist.
-    
-    :param path: The path to the directory.
-    :type path: str
-    :param name: The name of the directory. If None, the path will be used.
+
+    :param path: The path(s) to the directory.
+    :type path: tuple[str]
+    :param name: The name of the directory. Defaults to None.
     :type name: str | None
-    :return: Should return None.
-    :rtype: None
+    :return: Should return the path to the directory.
+    :rtype: str
     """
-    print(f"Creating directory for {name}")
-    if not os.path.exists(path):
-        os.makedirs(path)
+    target = os.path.join(get_hikaru_root(), *path)
+    if name:
+        print(f"Creating directory for {name} on {target}...")
     else:
-        print(f"Directory for {name} already exists.")
+        print(f"Creating directory on {target}...")
 
-def hide_unhide_directory(path, hide=True):
-    """
-    Hide or unhide a directory based on the platform.
+    if not os.path.exists(target):
+        os.makedirs(target)
 
-    :param path: Path to the directory
-    :param hide: True to hide, False to unhide
-    """
-    curr_os = system()
-    if curr_os == "Windows":
-        try:
-            # Get the current attributes
-            attributes = os.stat(path).st_file_attributes
-
-            # Set or clear the hidden attribute
-            if hide:
-                attributes |= 2  # Add the hidden attribute
-            else:
-                attributes &= ~2  # Remove the hidden attribute
-
-            # Set the new attributes
-            os.chflags(path, attributes)
-        except Exception as e:
-            print(f"Error: {e}")
-    elif curr_os == "Darwin":  # macOS
-        try:
-            # Set or clear the hidden flag using chflags
-            flag = "hidden" if hide else "nohidden"
-            os.system(f"chflags {flag} '{path}'")
-        except Exception as e:
-            print(f"Error: {e}")
+    if name and not os.path.exists(os.path.join(target, name)):
+        os.makedirs(os.path.join(target, name))
+        return os.path.join(target, name)
     else:
-        pass
+        return target
