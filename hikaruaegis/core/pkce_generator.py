d71bc1d837a092a1c0fd613ed514f62585764ef2
Natsu Tadama <nattadasu@nattadasu.my.id>
2023-12-17 21:49:26 +0700

    Prefer to use classes rather separated function

--- hikaruaegis/core/pkce_generator.py
+++ hikaruaegis/core/pkce_generator.py
@@ -3,58 +3,71 @@
 from base64 import urlsafe_b64encode
 from hashlib import sha256
 from secrets import token_urlsafe
-from typing import TypedDict
+from dataclasses import dataclass
 
 
-class PkceDict(TypedDict):
-    """A dictionary containing verifier and code challenge"""
+class LengthError(ValueError):
+    """Raised when length is not between 43 and 128 characters"""
+
+@dataclass
+class PkceCodes:
+    """A class containing verifier and code challenge"""
     verifier: str
     code_challenge: str
 
-def generate_pkce_code_verifier(length: int = 128) -> str:
-    """
-    Generate PKCE code verifier
 
-    :param length: Determine length of character to be generated
-    :type length: int = 128
-    :return: Code verifier
-    :rtype: str
-    """
-    # Generate a random string for code verifier
-    if length < 43 or length > 128:
-        raise ValueError("Code verifier length must be between 43 and 128 characters")
-    verifier = token_urlsafe(length)
-    return verifier[:length]
+class PkceGenerator:
+    """A class to generate PKCE codes"""
 
-def generate_pkce_code_challenge(verifier: str) -> str:
-    """
-    Generate code challenge if required
+    def __init__(self, length: int = 128):
+        """
+        Initialize the class
 
-    :param verifier: Code verifier generated from generate_pkce_code_verifier()
-    :type verifier: str
-    :return: Code challenge
-    :rtype: str
-    """
-    # Generate code challenge from code verifier using SHA-256
-    verifier_bytes = verifier.encode('ascii')
-    sha256_hash = sha256(verifier_bytes).digest()
-    code_challenge = urlsafe_b64encode(sha256_hash).rstrip(b'=').decode('utf-8')
-    return code_challenge
+        :param length: Determine length of character to be generated
+        :type length: int = 128
+        """
+        self.code_verifier = self._code_verifier(length=length)
+        self.code_challenge = self._code_challenge(self.code_verifier)
 
-def build_pcke_challenge(length: int = 128) -> PkceDict:
-    """
-    Generate a PCKE challenger, complete with verifier and code challenge 👍
+    @staticmethod
+    def _code_verifier(length: int = 128) -> str:
+        """
+        Generate PKCE code verifier
 
-    :param length: Determine length of character to be generated
-    :type length: int = 128
-    :return: Dictionary containing a verifier and code challenger
-    :rtype: PckeDict
-    """
+        :param length: Determine length of character to be generated
+        :type length: int = 128
+        :return: Code verifier
+        :rtype: str
+        """
+        # Generate a random string for code verifier
+        if length < 43 or length > 128:
+            raise LengthError(
+                "Code verifier length must be between 43 and 128 characters")
+        verifier = token_urlsafe(length)
+        return verifier[:length]
 
-    verify = generate_pkce_code_verifier(length=length)
-    final_dict: PkceDict = {
-        "verify": verify,
-        "code_challenge": generate_pkce_code_challenge(verify),
-    }
+    @staticmethod
+    def _code_challenge(verifier: str) -> str:
+        """
+        Generate code challenge if required
 
-    return final_dict
+        :param verifier: Code verifier generated from generate_pkce_code_verifier()
+        :type verifier: str
+        :return: Code challenge
+        :rtype: str
+        """
+        # Generate code challenge from code verifier using SHA-256
+        verifier_bytes = verifier.encode('ascii')
+        sha256_hash = sha256(verifier_bytes).digest()
+        code_challenge = urlsafe_b64encode(
+            sha256_hash).rstrip(b'=').decode('utf-8')
+        return code_challenge
+
+    def get_codes(self) -> PkceCodes:
+        """
+        Return PKCE codes
+
+        :return: PKCE codes
+        :rtype: PkceCodes
+        """
+        return PkceCodes(self.code_verifier, self.code_challenge)
