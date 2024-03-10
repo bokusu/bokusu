2a94558f9dd22b88f7d7cff6a1998342d149c179
Sultan Iskandar Maulana <nattadasu@nattadasu.my.id>
2024-03-10 10:06:49 +0000

    Fix read_resource won't load resource

--- bokusu/core/commons.py
+++ bokusu/core/commons.py
@@ -1,20 +1,32 @@
-from pkg_resources import resource_string
+from importlib import import_module
 
-def read_resource(path: str, file_name: str) -> str:
+class ResourceNotFoundError(Exception):
+    """Exception raised when a resource is not found."""
+
+def read_resource(directory: str, file_path: str, return_as_path: bool = False) -> str:
     """
-    Read a file from a resource
-
-    :param path: Path to the resource
-    :type path: str
-    :param file_name: File name
-    :type file_name: str
-
-    :return: File content
+    Gets the content of a file within a python package/module.
+    :param package_name: The name of the package.
+    :param file_path: The path to the file within the package.
+    :param return_as_path: If True, the file path is returned instead of the file content.
+    :type package_name: str
+    :type file_path: str
+    :type return_as_path: bool
+    :return: The content of the file.
     :rtype: str
+    :raises ResourceNotFoundError: If the file is not found.
+    :example:
+    >>> read_resource('bokusu', 'core/commons.py')
     """
-
-    # load file from module's directory (hikaru/{path}/{file_name})
-    file_content = resource_string(path, file_name).decode("utf-8")
-
-    # return the file content
-    return file_content
+    try:
+        package = import_module("bokusu")
+        module_path = package.__path__[0]
+        directory = directory.replace(".", "/")
+        directory = directory.replace("\\", "/")
+        full_file_path = f"{module_path}/{directory}/{file_path}"
+        if return_as_path:
+            return full_file_path
+        with open(full_file_path, 'r') as f:
+            return f.read()
+    except (ImportError, FileNotFoundError):
+        raise ResourceNotFoundError(f"File '{file_path}' not found inside bokusu package.")
