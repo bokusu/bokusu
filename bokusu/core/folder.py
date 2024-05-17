2872a5abea759282a268fc9092077f5263474242
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-05-17 11:31:23 +0700

    Refactor folder core module

--- bokusu/core/folder.py
+++ bokusu/core/folder.py
@@ -1,40 +1,45 @@
 """This module contains functions for setting up folders."""
 
 import os
-from platform import system
+from pathlib import Path
 
-from bokusu.core.const import OVERRIDE_PATH
+from bokusu.core.const import GITHUB_ACTIONS, OVERRIDE_PATH, Status, pp
 
 
-def get_box_root() -> str:
+def create_folder(*path: str) -> Path:
+    """
+    Creates a folder if it doesn't exist.
+
+    :param path: The path(s) to the folder.
+    :type path: tuple[str]
+    :return: The path to the folder.
+    :rtype: Path
+    """
+    target = os.path.join(*path)
+    os.makedirs(target, exist_ok=True)
+    return Path(target).resolve()
+
+
+def get_box_root() -> Path:
     """
     Get the root directory of Bokusu.
 
-    On Windows, it should be %USERPROFILE%\\\\.bokusu, but on POSIX, it should be ~/.bokusu.
-    However, on CI/CD (GitHub Actions), it should be current repo directory.
+    On Windows, it should be %USERPROFILE%\\.bokusu, but on POSIX, it should be ~/.bokusu.
+    However, on CI/CD (GitHub Actions), it should be the current repo directory.
 
     :return: The path to the root directory.
-    :rtype: str
+    :rtype: Path
     """
-    # check if the program runs on Windows
-    if system() == "Windows":
-        # get the user profile directory
-        user_profile = os.getenv("USERPROFILE")
-        # check if the user profile directory exists
-        if user_profile:
-            # return the path to the root directory
-            return os.path.join(user_profile, ".bokusu")
-        # return the path to the root directory
-        return os.path.join(os.getcwd(), ".bokusu")
-    if os.getenv("GITHUB_ACTIONS"):
-        return os.getcwd()
-    # resolve the home directory
-    home = os.path.expanduser("~")
-    # return the path to the root directory
-    return os.path.join(home, ".bokusu")
+    if GITHUB_ACTIONS:
+        return Path(".").resolve()
+    path = Path("~/.bokusu").expanduser()
+    if path.exists():
+        return path
+    pp.print(Status.ERROR, "Bokusu root directory not found. Creating one...")
+    return create_folder(str(path))
 
 
-def add_directory(*path: str, name: str | None = None) -> str:
+def add_directory(*path: str, name: str | None = None) -> Path:
     """
     Creates a directory if it doesn't exist.
 
@@ -42,20 +47,16 @@ def add_directory(*path: str, name: str | None = None) -> str:
     :type path: tuple[str]
     :param name: The name of the directory. Defaults to None.
     :type name: str | None
-    :return: Should return the path to the directory.
-    :rtype: str
+    :return: The path to the directory.
+    :rtype: Path
     """
-    root = OVERRIDE_PATH or get_box_root()
-    target = os.path.join(root, *path)
+    root = Path(OVERRIDE_PATH) or get_box_root()
+    target = os.path.join(str(root), *path)
     if name:
-        print(f"Creating directory for {name} on {target}...")
+        pp.print(Status.INFO, f"Creating directory for {name} on {target}...")
     else:
-        print(f"Creating directory on {target}...")
+        pp.print(Status.INFO, f"Creating directory on {target}...")
 
-    if not os.path.exists(target):
-        os.makedirs(target)
+    target = create_folder(target)
 
-    if name and not os.path.exists(os.path.join(target, name)):
-        os.makedirs(os.path.join(target, name))
-        return os.path.join(target, name)
     return target
