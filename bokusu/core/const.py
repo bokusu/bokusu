f9d64725445534bb518b4ae0bb36e0cd37cefd9e
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-05-22 17:54:05 +0700

    Add typings and initiate librensetsu's PrettyPrint

--- bokusu/core/const.py
+++ bokusu/core/const.py
@@ -1,5 +1,9 @@
 from os import getenv as env
 from os import path
+from pathlib import Path
+
+from librensetsu.const import IS_GITHUB_WORKFLOW  # type: ignore
+from librensetsu.prettyprint import Platform, PrettyPrint, Status  # type: ignore
 
 # script path
 __file__: str = path.abspath(__name__)
@@ -10,6 +14,27 @@ IS_VERBOSE: bool = False
 """Check if the program runs in verbose mode"""
 
 USER_AGENT: str = env("USER_AGENT") or ""
+"""User agent for HTTP requests"""
 
-OVERRIDE_PATH: str = env("OVERRIDE_PATH") or ""
+GITHUB_ACTIONS: bool = IS_GITHUB_WORKFLOW
+"""Check if the program runs on GitHub Actions"""
+
+
+OVERRIDE_PATH: Path | str = ""
 """The path to the user's configured folder to store data"""
+
+CONFIG_PATH: Path | str = ""
+"""The path to the configuration file"""
+
+pp = PrettyPrint(Platform.SYSTEM)
+"""Pretty print object"""
+
+__all__ = [
+    "IS_VERBOSE",
+    "USER_AGENT",
+    "GITHUB_ACTIONS",
+    "OVERRIDE_PATH",
+    "CONFIG_PATH",
+    "pp",
+    "Status",
+]
